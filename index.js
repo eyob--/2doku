2fe85bbaee597e5794162a0814a1b103b2a97e65
eyob-- <tse.eyob@gmail.com>
2015-06-28 15:16:11 -0400

    Initial 2-person room layout
    current commit is not functional

--- index.js
+++ index.js
@@ -8,10 +8,57 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
+
+var rooms = [];
+var roomNames = [];
+var fullRooms = [];
+
+function Room(name, socket1, socket2) {
+  this.name = name;
+  this.socket1 = socket1;
+  this.socket2 = socket2;
+}
+Room.prototype.isEmpty = function() {
+  return this.socket1 === null && this.socket2 === null;
+}
+
 io.on('connection', function(socket) {
   console.log('user connection');
+  console.log(fullRooms);
+  io.emit('rooms', {arr: fullRooms});
+
+  var room = null;
+
+  socket.on('joined', function(roomName) {
+    console.log(roomName + ' joined');
+    if (roomNames.indexOf(roomName) === -1) {
+      room = new Room(roomName, socket, null);
+      rooms.push(room);
+      roomNames.push(roomName);
+    }
+    else {
+      var i = roomNames.indexOf(roomName);
+      rooms[i].socket2 = socket;
+      room = rooms[i];
+      fullRooms.push(roomName);
+    }
+  });
 
   socket.on('disconnect', function() {
+    if (room.socket1 === socket) {
+      room.socket1 === null;
+    }
+    else {
+      room.socket2 === null;
+    }
+    if (fullRooms.indexOf(room.name) !== -1) {
+      fullRooms.splice(fullRooms.indexOf(room.name), 1);
+    }
+    if (room.isEmpty()) {
+      var i = roomNames.indexOf(room.name);
+      roomNames.splice(i, 1);
+      rooms.splice(i, 1);
+    }
     console.log('user disconnection');
   });
 
--- public/js/client.js
+++ public/js/client.js
@@ -1,1 +1,17 @@
 var socket = io();
+
+var fullRooms = null;
+
+socket.on('rooms', function(rooms) {
+  rooms = rooms['arr'];
+  console.log('rooms received: '+rooms);
+  fullRooms = rooms;
+});
+
+var roomName = prompt('Join a room!');
+while(fullRooms === null || fullRooms.indexOf(roomName) !== -1) {
+  console.log(fullRooms);
+  var roomName = prompt('Sorry, that room is taken! Join another room!');
+}
+
+socket.emit('joined', roomName);
