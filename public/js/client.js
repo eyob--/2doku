793be4c899806491af835e958549e1fd5da9d1dd
edge <sort@programmer.net>
2015-06-29 06:00:09 +0900

    move room prompt into callback

--- public/js/client.js
+++ public/js/client.js
@@ -6,12 +6,12 @@ socket.on('rooms', function(rooms) {
   rooms = rooms['arr'];
   console.log('rooms received: '+rooms);
   fullRooms = rooms;
+  var roomName = prompt('Join a room!');
+  console.log(roomName)
+  while(fullRooms === null || fullRooms.indexOf(roomName) !== -1) {
+    console.log(fullRooms);
+    var roomName = prompt('Sorry, that room is taken! Join another room!');
+  }
 });
-
-var roomName = prompt('Join a room!');
-while(fullRooms === null || fullRooms.indexOf(roomName) !== -1) {
-  console.log(fullRooms);
-  var roomName = prompt('Sorry, that room is taken! Join another room!');
-}
 
 socket.emit('joined', roomName);
