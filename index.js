ec29882958a282304295302939b9831d82ea1365
edge <sort@programmer.net>
2015-06-29 06:04:13 +0900

    clean up code

--- index.js
+++ index.js
@@ -1,69 +1,70 @@
 var express = require('express');
 var app = express();
-app.use(express.static('./public'));
 var srv = require('http').Server(app);
 var io = require('socket.io')(srv);
 
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/index.html');
-});
+app.use(express.static('./public'));
 
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/index.html');
+});
 
 var rooms = [];
 var roomNames = [];
 var fullRooms = [];
 
 function Room(name, socket1, socket2) {
-  this.name = name;
-  this.socket1 = socket1;
-  this.socket2 = socket2;
+	this.name = name;
+	this.socket1 = socket1;
+	this.socket2 = socket2;
 }
+
 Room.prototype.isEmpty = function() {
-  return this.socket1 === null && this.socket2 === null;
+	return this.socket1 === null && this.socket2 === null;
 }
 
 io.on('connection', function(socket) {
-  console.log('user connection');
-  console.log(fullRooms);
-  io.emit('rooms', {arr: fullRooms});
+	console.log('user connection');
+	console.log(fullRooms);
+	io.emit('rooms', { arr: fullRooms });
 
-  var room = null;
+	var room = null;
 
-  socket.on('joined', function(roomName) {
-    console.log(roomName + ' joined');
-    if (roomNames.indexOf(roomName) === -1) {
-      room = new Room(roomName, socket, null);
-      rooms.push(room);
-      roomNames.push(roomName);
-    }
-    else {
-      var i = roomNames.indexOf(roomName);
-      rooms[i].socket2 = socket;
-      room = rooms[i];
-      fullRooms.push(roomName);
-    }
-  });
+	socket.on('joined', function(roomName) {
+		console.log(roomName + ' joined');
+		if (!~roomNames.indexOf(roomName)) {
+			room = new Room(roomName, socket, null);
+			rooms.push(room);
+			roomNames.push(roomName);
+		}
+		else {
+			var i = roomNames.indexOf(roomName);
+			rooms[i].socket2 = socket;
+			room = rooms[i];
+			fullRooms.push(roomName);
+		}
+	});
 
-  socket.on('disconnect', function() {
-    if (room.socket1 === socket) {
-      room.socket1 === null;
-    }
-    else {
-      room.socket2 === null;
-    }
-    if (fullRooms.indexOf(room.name) !== -1) {
-      fullRooms.splice(fullRooms.indexOf(room.name), 1);
-    }
-    if (room.isEmpty()) {
-      var i = roomNames.indexOf(room.name);
-      roomNames.splice(i, 1);
-      rooms.splice(i, 1);
-    }
-    console.log('user disconnection');
-  });
+	socket.on('disconnect', function() {
+		if (room.socket1 === socket) {
+			room.socket1 === null;
+		}
+		else {
+			room.socket2 === null;
+		}
+		if (fullRooms.indexOf(room.name) !== -1) {
+			fullRooms.splice(fullRooms.indexOf(room.name), 1);
+		}
+		if (room.isEmpty()) {
+			var i = roomNames.indexOf(room.name);
+			roomNames.splice(i, 1);
+			rooms.splice(i, 1);
+		}
+		console.log('user disconnection');
+	});
 
 });
 
 srv.listen(3000, function() {
-  console.log('listening on *:3000');
+	console.log('listening on *:3000');
 });
--- public/js/client.js
+++ public/js/client.js
@@ -1,17 +1,20 @@
-var socket = io();
+(function() {
+	var socket = io();
 
-var fullRooms = null;
+	var fullRooms = null;
 
-socket.on('rooms', function(rooms) {
-  rooms = rooms['arr'];
-  console.log('rooms received: '+rooms);
-  fullRooms = rooms;
-  var roomName = prompt('Join a room!');
-  console.log(roomName)
-  while(fullRooms === null || fullRooms.indexOf(roomName) !== -1) {
-    console.log(fullRooms);
-    var roomName = prompt('Sorry, that room is taken! Join another room!');
-  }
-});
+	socket.on('rooms', function(rooms) {
+		rooms = rooms['arr'];
+		console.log('rooms received: '+rooms);
+		fullRooms = rooms;
+		var roomName = prompt('Join a room!');
+		console.log(roomName)
+		while(fullRooms === null || fullRooms.indexOf(roomName) !== -1) {
+			console.log(fullRooms);
+			var roomName = prompt('Sorry, that room is taken! Join another room!');
+		}
+	});
 
-socket.emit('joined', roomName);
+	socket.emit('joined', roomName);
+
+})();
